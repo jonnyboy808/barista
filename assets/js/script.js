cf09b8821a1ab7158bf8210982ac16878a4070ca
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-11 09:37:58 -0700

    created search radius

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,21 @@
     
     
     
-   
+    
     function loadMapScenario() {
         var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
             center: new Microsoft.Maps.Location(37.7749, -122.4194),
             zoom: 13
         });
+
+        var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+
+        Microsoft.Maps.Events.addHandler(map, 'click', function (e) {
+            searchManager.reverseGeocode({
+                location: e.location,
+                callback: function (result) {
+                    if (result && result.address) {
+                        var searchRequest = {
+                            query: 'coffee',
+                            location: e.location,
+                            radius: 500,
