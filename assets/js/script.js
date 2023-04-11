c287cc84731ff1094d2baf90ce571cb01c64a8d6
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-11 09:30:44 -0700

    created function for map display

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+    
+    
+    
+    
+    
+   
+    function loadMapScenario() {
+        var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+            center: new Microsoft.Maps.Location(37.7749, -122.4194),
+            zoom: 13
+        });
