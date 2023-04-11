e2ab3fbfdd4740336a629f36d9980907ca6f0959
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-11 09:42:46 -0700

    added call back function for response

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@
     
     
     
-    
-    
+
     function loadMapScenario() {
         var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
             center: new Microsoft.Maps.Location(37.7749, -122.4194),
@@ -22,3 +21,10 @@
                             query: 'coffee',
                             location: e.location,
                             radius: 500,
+                            callback: function (searchResponse, searchRequest) {
+                                if (searchResponse && searchResponse.results) {
+                                    var coffeeLayer = new Microsoft.Maps.Layer();
+                                    for (var i = 0; i < searchResponse.results.length; i++) {
+                                        var coffeePushpin = new Microsoft.Maps.Pushpin(searchResponse.results[i].location, { title: searchResponse.results[i].name });
+                                        coffeeLayer.add(coffeePushpin);
+                                 
