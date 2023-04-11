676d36e1421b1dc2c7ad34615314168923796748
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-11 13:29:38 -0700

    added corrected map function and commented out previous map js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,77 @@ function handleCallingApis(){
 }
 // Weather widget only visible when city entered in search bar or when city from previously searched list is clicked
     
-    
+      var map, searchManager;
+
+    function GetMap() {
+        map = new Microsoft.Maps.Map('#myMap', {
+            credentials: 'Your Bing Maps Key'
+        });
+    }
+
+    function Search() {
+        if (!searchManager) {
+            //Create an instance of the search manager and perform the search.
+            Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+                searchManager = new Microsoft.Maps.Search.SearchManager(map);
+                Search()
+            });
+        } else {
+            //Remove any previous results from the map.
+            map.entities.clear();
+
+            //Get the users query and geocode it.
+            var query = document.getElementById('searchTbx').value;
+            geocodeQuery(query);
+        }
+    }
+
+    function geocodeQuery(query) {
+        var searchRequest = {
+            where: query,
+            callback: function (r) {
+                if (r && r.results && r.results.length > 0) {
+                    var pin, pins = [], locs = [], output = 'Results:<br/>';
+
+                    for (var i = 0; i < r.results.length; i++) {
+                        //Create a pushpin for each result. 
+                        pin = new Microsoft.Maps.Pushpin(r.results[i].location, {
+                            text: i + ''
+                        });
+                        pins.push(pin);
+                        locs.push(r.results[i].location);
+
+                        output += i + ') ' + r.results[i].name + '<br/>';
+                    }
+
+                    //Add the pins to the map
+                    map.entities.push(pins);
+
+                    //Display list of results
+                    document.getElementById('output').innerHTML = output;
+
+                    //Determine a bounding box to best view the results.
+                    var bounds;
+
+                    if (r.results.length == 1) {
+                        bounds = r.results[0].bestView;
+                    } else {
+                        //Use the locations from the results to calculate a bounding box.
+                        bounds = Microsoft.Maps.LocationRect.fromLocations(locs);
+                    }
+
+                    map.setView({ bounds: bounds });
+                }
+            },
+            errorCallback: function (e) {
+                //If there is an error, alert the user about it.
+                alert("No results found.");
+            }
+        };
+
+        //Make the geocode request.
+        searchManager.geocode(searchRequest);
+    }
     
     
     
