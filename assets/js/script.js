066e7c7d26cb1e7bbccfab8ed256829f2b88aab3
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-11 10:07:23 -0700

    fix conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+var googleapiKey = "AIzaSyBxttPCiINqjGfmC3mQcs-MznhmsLTwjyQ"
 
+// Weather widget only visible when city entered in search bar or when city from previously searched list is clicked
     
     
     
